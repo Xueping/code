25dace904554de5f5cd694df26aea28c6970363a
Xueping <xuepingpeng@yahoo.com>
2014-03-25 13:56:44 +1100

    add new two beans to store category and programming language

---
+++ codesaleReview/src/uts/codesale/beans/Language.java
@@ -0,0 +1,32 @@
+package uts.codesale.beans;
+
+import java.io.Serializable;
+
+import uts.codesale.commons.AGenericObject;
+
+public class Language extends AGenericObject implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- codesaleReview/src/uts/codesale/beans/Lookup.java
+++ codesaleReview/src/uts/codesale/beans/Lookup.java
@@ -23,8 +23,6 @@ public class Lookup extends AGenericObject implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
-	
-	
 	private String type;
 	private String value;
 
