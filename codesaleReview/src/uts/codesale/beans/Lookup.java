a87084901f3912f88bfc44844a0d37a9ba4ec211
Xueping <xuepingpeng@yahoo.com>
2014-03-25 13:58:23 +1100

    Revert "add new two beans to store category and programming language"
    This reverts commit 25dace904554de5f5cd694df26aea28c6970363a.

--- codesaleReview/src/uts/codesale/beans/Lookup.java
+++ codesaleReview/src/uts/codesale/beans/Lookup.java
@@ -23,6 +23,8 @@ public class Lookup extends AGenericObject implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	
+	
 	private String type;
 	private String value;
 
